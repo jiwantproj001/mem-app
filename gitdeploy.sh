6e0a2679b6c3e5f398f8e9c0921f5abfa872ea82
Jiwant <jiwantproj001@gmail.com>
2020-11-01 15:06:31 -0500

    Deploy to GitHub Pages

--- gitdeploy.sh
+++ gitdeploy.sh
@@ -2,7 +2,7 @@ git config --global user.name "Jiwant"
 git config --global user.email jiwantproj001@gmail.com
 npm run build
 git add .
-git commit -m 'deploy'
+git commit -m 'Deploy to GitHub Pages'
 git subtree split --prefix dist -b gh-pages
 git push -f proj gh-pages:gh-pages
 git branch -D gh-pages
